ae512c357961b70f26860d0d15f935440e4bef89
Adeola Adetoyese <adeolakautharadetoyese@mitt.ca>
2022-05-15 19:51:17 -0500

    Create Project Controller

--- BugTracker/BLL/ProjectBusinessLogic.cs
+++ BugTracker/BLL/ProjectBusinessLogic.cs
@@ -11,5 +11,33 @@ namespace BugTracker.BLL
         {
             repo = repoArg;
         }
+
+        public List<Projects> AllProjects()
+        {
+            return repo.GetAll().ToList();
+        }
+
+        public Projects GetProject(int id)
+        {
+            return repo.Get(id);
+        }
+
+        public void CreateProject(Projects project)
+        {
+            repo.Add(project);
+            repo.Save();
+        }
+
+        public void UpdateProject(Projects project)
+        {
+            repo.Update(project);
+            repo.Save();
+        }
+
+        public void DeleteProject(Projects project)
+        {
+            repo.Delete(project);
+            repo.Save();
+        }
     }
 }
