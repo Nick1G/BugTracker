ec4aaa8f0c818f7845c42c3b4204c8cdcde961d5
Nicholas Greschuk <nicholasgreschuk@student.mitt.ca>
2022-05-19 01:52:10 -0500

    Creating basic initialize method in projectBL tests

--- BugTrackerTesting/Usings.cs
+++ BugTrackerTesting/Usings.cs
@@ -1,1 +1,6 @@
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
+global using Moq;
+global using BugTracker.BLL;
+global using BugTracker.Data;
+global using BugTracker.DAL;
+global using BugTracker.Models;
