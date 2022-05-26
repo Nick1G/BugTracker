c25a8547cd2d0f817bbf7543f9a5fd94ef86008d
Nicholas Greschuk <nicholasgreschuk@student.mitt.ca>
2022-05-26 02:08:50 -0500

    Creating test for GetProjectsList

--- BugTrackerTesting/ProjectBLTests.cs
+++ BugTrackerTesting/ProjectBLTests.cs
@@ -5,17 +5,13 @@ namespace BugTrackerTesting
     {
         private ProjectBusinessLogic projectBL;
         private List<Projects> allProjects;
-        private Projects testAdd;
 
         [TestInitialize]
         public void Initialize()
         {
-            Projects mockProject1 = new Projects() { Id = 1, Name = "Project1", };
-            Projects mockProject2 = new Projects() { Id = 2, Name = "Project2", };
+            Projects mockProject1 = new Projects() { Id = 1, Name = "Project", };
+            Projects mockProject2 = new Projects() { Id = 2, Name = "Project", };
             Projects mockProject3 = new Projects() { Id = 3, Name = "Project3", };
-            Projects testAddProject = new Projects() { Id = 4, Name = "Newton" };
-
-            testAdd = testAddProject;
 
             allProjects = new List<Projects>() { mockProject1, mockProject2, mockProject3, };
 
@@ -24,10 +20,12 @@ namespace BugTrackerTesting
             mockRepo.Setup(repo => repo.Get(It.Is<int>(id => id == 1))).Returns(mockProject1);
             mockRepo.Setup(repo => repo.Get(It.Is<int>(id => id == 2))).Returns(mockProject2);
             mockRepo.Setup(repo => repo.Get(It.Is<int>(id => id == 3))).Returns(mockProject3);
+
             mockRepo.Setup(repo => repo.GetAll()).Returns(allProjects);
+            mockRepo.Setup(repo => repo.GetList(It.IsAny<Func<Projects, bool>>())).Returns<Func<Projects, bool>>((func) => allProjects.Where(func).ToList());
 
             mockRepo.Setup(repo => repo.Delete(It.Is<Projects>(project => project == mockProject1))).Callback(() => allProjects.Remove(mockProject1));
-            mockRepo.Setup(repo => repo.Add(It.Is<Projects>(project => project == testAddProject))).Callback(() => allProjects.Add(testAddProject));
+            mockRepo.Setup(repo => repo.Add(It.IsAny<Projects>())).Callback((Projects e) => allProjects.Add(e));
 
             projectBL = new ProjectBusinessLogic(mockRepo.Object);
         }
@@ -60,8 +58,23 @@ namespace BugTrackerTesting
         [TestMethod]
         public void CreateProjectTest()
         {
+            var testAdd = new Projects();
             projectBL.CreateProject(testAdd);
             CollectionAssert.Contains(allProjects, testAdd);
+        }
+
+        [TestMethod]
+        public void GetProjectsListTest()
+        {
+            var proj1 = projectBL.GetProject(1);
+            var proj2 = projectBL.GetProject(2);
+            var proj3 = projectBL.GetProject(3);
+
+            var whereList = projectBL.GetProjectsList(project => project.Name == "Project");
+
+            CollectionAssert.Contains(whereList, proj1);
+            CollectionAssert.Contains(whereList, proj2);
+            CollectionAssert.DoesNotContain(whereList, proj3);
         }
     }
 }
