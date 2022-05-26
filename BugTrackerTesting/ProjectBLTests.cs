520cafadd4232c8f2847fd737f2aa4befe3a880f
Nicholas Greschuk <nicholasgreschuk@student.mitt.ca>
2022-05-26 09:38:45 -0500

    Adding test got GetAssignedProjects

--- BugTrackerTesting/ProjectBLTests.cs
+++ BugTrackerTesting/ProjectBLTests.cs
@@ -5,6 +5,7 @@ namespace BugTrackerTesting
     {
         private ProjectBusinessLogic projectBL;
         private List<Projects> allProjects;
+        private ApplicationUser user;
 
         [TestInitialize]
         public void Initialize()
@@ -12,8 +13,13 @@ namespace BugTrackerTesting
             Projects mockProject1 = new Projects() { Id = 1, Name = "Project", };
             Projects mockProject2 = new Projects() { Id = 2, Name = "Project", };
             Projects mockProject3 = new Projects() { Id = 3, Name = "Project3", };
+            ApplicationUser newUser = new ApplicationUser();
+
+            ProjectUsers projUser1 = new ProjectUsers(mockProject1.Id, newUser.Id);
+            ProjectUsers projUser2 = new ProjectUsers(mockProject3.Id, newUser.Id);
 
             allProjects = new List<Projects>() { mockProject1, mockProject2, mockProject3, };
+            user = newUser;
 
             Mock<IRepository<Projects>> mockRepo = new Mock<IRepository<Projects>>();
 
@@ -25,7 +31,7 @@ namespace BugTrackerTesting
             mockRepo.Setup(repo => repo.GetList(It.IsAny<Func<Projects, bool>>())).Returns<Func<Projects, bool>>((func) => allProjects.Where(func).ToList());
 
             mockRepo.Setup(repo => repo.Delete(It.Is<Projects>(project => project == mockProject1))).Callback(() => allProjects.Remove(mockProject1));
-            mockRepo.Setup(repo => repo.Add(It.IsAny<Projects>())).Callback((Projects e) => allProjects.Add(e));
+            mockRepo.Setup(repo => repo.Add(It.IsAny<Projects>())).Callback((Projects proj) => allProjects.Add(proj));
 
             projectBL = new ProjectBusinessLogic(mockRepo.Object);
         }
@@ -66,15 +72,29 @@ namespace BugTrackerTesting
         [TestMethod]
         public void GetProjectsListTest()
         {
-            var proj1 = projectBL.GetProject(1);
-            var proj2 = projectBL.GetProject(2);
-            var proj3 = projectBL.GetProject(3);
+            var proj1 = allProjects[0];
+            var proj2 = allProjects[1];
+            var proj3 = allProjects[2];
 
             var whereList = projectBL.GetProjectsList(project => project.Name == "Project");
 
             CollectionAssert.Contains(whereList, proj1);
             CollectionAssert.Contains(whereList, proj2);
             CollectionAssert.DoesNotContain(whereList, proj3);
+        }
+
+        [TestMethod]
+        public void GetAssignedProjectsTest()
+        {
+            var proj1 = allProjects[0];
+            var proj2 = allProjects[1];
+            var proj3 = allProjects[2];
+
+            var assignedList = projectBL.GetAssignedProjects(user);
+
+            CollectionAssert.Contains(assignedList, proj1);
+            CollectionAssert.Contains(assignedList, proj3);
+            CollectionAssert.DoesNotContain(assignedList, proj2);
         }
     }
 }
